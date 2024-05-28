26a8a5538cdbaecd1ff4b0496feea115151f73da
Hojaverde <jmprocco@gmail.com>
2024-05-27 22:01:34 -0400

    Added compileShaders.ps1

---
+++ Build/Assets/Shaders/_compile.bat
@@ -0,0 +1,1 @@
+powershell -noexit -File ./compileShaders.ps1
---
+++ Build/Assets/Shaders/compileShaders.ps1
@@ -0,0 +1,14 @@
+# Get all .vert and .frag files in the current directory
+#$files = Get-ChildItem -Path ./ -Filter "*" Where-Object { $_.Name -match '*.vert|*.frag' }
+$files = Get-ChildItem -Filter "*" .\* | Where-Object { $_.Name -match '^*.vert|^*.frag' }
+# Loop over each file
+foreach ($file in $files) {
+    # Construct the output file path
+
+    Write-Output "Compiling $file"
+
+    $outputFile = ".\Compiled\$($file.Name).spv"
+
+    # Run the command with the current file
+    & "C:\VulkanSDK\1.3.268.0\Bin\glslc.exe" "$($file.FullName)" -o $outputFile
+}
