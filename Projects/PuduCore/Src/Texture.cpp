e23971e3eea894a24bf4a6279166fb20e4fdcc4b
Hojaverde <jmprocco@gmail.com>
2025-06-10 07:01:25 -0400

    Revert "clamped texture calculate mip level to max mip levels"
    This reverts commit a5879be512092fb7fd2b02d1bbb185b3c4e3928c.

--- Projects/PuduCore/Src/Texture.cpp
+++ Projects/PuduCore/Src/Texture.cpp
@@ -78,6 +78,6 @@ namespace Pudu
 
     u32 Texture::CalculateMipLevels(u32 width, u32 height)
     {
-        return min( static_cast<u32>(std::floor(std::log2(std::max(width,height))))+ 1,K_MAX_MIP_LEVELS);
+        return static_cast<u32>(std::floor(std::log2(std::max(width,height)))) + 1;
     }
 }
